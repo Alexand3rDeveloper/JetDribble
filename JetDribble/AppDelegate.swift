5d4cd7ee5fefe46b6990025f4c2ebcae03f07cc1
Alexand3rDeveloper <alexappledeveloper@gmail.com>
2016-12-19 19:45:40 +0300

    no message

--- JetDribble/AppDelegate.swift
+++ JetDribble/AppDelegate.swift
@@ -19,15 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-//application:openURL:sourceApplication:annotation
-    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        print(url)
-        return true
-    }
-//    - (BOOL)application:(UIApplication *)application
-//    openURL:(NSURL *)url
-//    sourceApplication:(NSString *)sourceApplication
-//    annotation:(id)annotation
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
